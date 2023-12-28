aff8b9c753d10f89061138efe1cf7c09cb75f543
Dmitriy <dim116@inbox.ru>
2023-12-28 15:49:35 +0300

    added rotate all view

--- app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
+++ app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
@@ -32,6 +32,7 @@ class StatsView @JvmOverloads constructor(
     private var progress = 0F
     private var valueAnimator: ValueAnimator? = null
 
+
     init {
         context.withStyledAttributes(attrs, R.styleable.StatsView) {
             lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
@@ -77,7 +78,7 @@ class StatsView @JvmOverloads constructor(
         for ((index, datum) in data.withIndex()) {
             val angle = 360F * datum
             paint.color = colors.getOrNull(index) ?: randomColor()
-            canvas.drawArc(oval, startFrom, angle * progress, false, paint)
+            canvas.drawArc(oval, (startFrom+360*progress), angle * progress, false, paint)
             startFrom += angle
         }
 
@@ -101,7 +102,7 @@ class StatsView @JvmOverloads constructor(
                 progress = anim.animatedValue as Float
                 invalidate()
             }
-            duration = 500
+            duration = 5000
             interpolator = LinearInterpolator()
         }.also {
             it.start()
